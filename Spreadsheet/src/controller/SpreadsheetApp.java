e8c9d58fddd11042361fd6b4cf4f70fcf023d5bc
Patribird <phern6@yahoo.com>
2023-03-02 22:42:54 -0800

    Commit to my own branch, so I can download the update project files.

--- Spreadsheet/src/controller/SpreadsheetApp.java
+++ Spreadsheet/src/controller/SpreadsheetApp.java
@@ -103,7 +103,7 @@ public class SpreadsheetApp {
 
         System.out.println("Enter the cell's new formula: ");
         inputFormula = readString();
-        //expTreeTokenStack = getFormula(inputFormula);
+        expTreeTokenStack = getFormula(inputFormula);
 
         /*
         // This code prints out the expression stack from
@@ -115,7 +115,7 @@ public class SpreadsheetApp {
         }
         */
 
-        //theSpreadsheet.changeCellFormulaAndRecalculate(cellToken, expTreeTokenStack);
+        theSpreadsheet.changeCellFormulaAndRecalculate(cellToken, expTreeTokenStack);
         System.out.println();
     }
 
