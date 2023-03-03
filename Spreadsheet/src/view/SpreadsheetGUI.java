3c02d147d65a9688005d30035e094c2b1b1d849f
montg <montgomerynathameion@gmail.com>
2023-03-02 23:26:51 -0800

    Started work on the GUI

--- Spreadsheet/src/view/SpreadsheetGUI.java
+++ Spreadsheet/src/view/SpreadsheetGUI.java
@@ -1,4 +1,32 @@
 package model.Spreadsheet.src.view;
 
-public class SpreadsheetGUI {
+import javax.swing.*;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class SpreadsheetGUI extends JFrame implements PropertyChangeListener {
+
+    /**
+     * The users screen size.
+     */
+    private static final Dimension SCREEN_SIZE = KIT.getScreenSize();
+
+    /**
+     * This method gets called when a bound property is changed.
+     *
+     * @param evt A PropertyChangeEvent object describing the event source
+     *            and the property that has changed.
+     */
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
+
+    private void setupGUI() {
+        setTitle("Spreadsheet");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(new Dimension(SCREEN_SIZE));
+    }
 }
