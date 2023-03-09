c1d0a54d38ad14d40c13e5171907d894b4e38277
applestopearss <timratsko909@gmail.com>
2023-03-08 17:29:39 -0800

    Added some javadoc comments.

--- Spreadsheet/src/model/Cell.java
+++ Spreadsheet/src/model/Cell.java
@@ -15,8 +15,9 @@ import java.util.Stack;
  * @author Moon Chang
  */
 public class Cell {
+	/** The row this cell is in. */
 	private final int myRow;
-
+	/** The column this cell is in. */
 	private final int myCol;
 	/** The formula of the cell. */
 	private String myFormula;
@@ -26,17 +27,23 @@ public class Cell {
 	private ExpressionTree myExpressionTree;
 	/** The formula in order of how it was typed. */
 	private String myInOrderFormula;
-
+	/** The root node of the tree that holds the formula for this cell. */
 	private ExpressionTreeNode myTreeNodeRoot;
-
+	/** The list of cells used inside this cells` formula. */
 	private final ArrayList<Cell> cellsInMyFormula = new ArrayList<Cell>();
-
+	/** The list of cells that use this cell in their formula. */
 	private final ArrayList<Cell> cellsThatContainMeInFormula = new ArrayList<Cell>();
 
+	/** Create a cell using an int for row and column.
+	 *
+	 * @param theRow The row this cell is in.
+	 * @param theCol The column this cell is in.
+	 */
 	public Cell(int theRow, int theCol) {
 		myRow = theRow;
 		myCol = theCol;
 	}
+
 	/**
 	 * Sets the formula of the Cell to the formula provided.
 	 * @param theFormula The formula for the cell to be set to.
@@ -62,7 +69,7 @@ public class Cell {
 	/** Sets the tree node root as the formula that should be evaluated.
 	 *
 	 * @param theExpTreeNode The tree node that has the entire expression.
-	 */ // This is here because there were issues with the formula.
+	 */
 	public void setMyExpressionTree(ExpressionTreeNode theExpTreeNode) {
 		myTreeNodeRoot = theExpTreeNode;
 	}
@@ -110,14 +117,27 @@ public class Cell {
 		return myValue;
 	}
 
+	/** Gets all the cells in the formula.
+	 *
+	 * @return A list of the cells in this formula.
+	 */
 	public ArrayList<Cell> getCellsInMyFormula() {
 		return cellsInMyFormula;
 	}
 
+	/** Gets the cells that contain this formula.
+	 *
+	 * @return A list of the cells that contain this formula.
+	 */
 	public ArrayList<Cell> getCellsThatContainThisInFormula() {
 		return cellsThatContainMeInFormula;
 	}
 
+	/** Creates a list of Prerequisites for this cell and adds it to the cellsInMyFormula list
+	 *
+	 * @param formula The formula of this list.
+	 * @param theSpreadsheet The main spreadsheet.
+	 */
 	public void createListOfPrerequisites(String formula, Spreadsheet theSpreadsheet) {
 		char ch = ' ';
 		CellToken cellToken;
@@ -155,15 +175,27 @@ public class Cell {
 		}
 	}
 
+	/**
+	 *  Clears the lists of cells that contain this cell in their formula,
+	 *  and the cells that are contained in this cells' formula.
+	 */
 	public void clearLists() {
 		cellsThatContainMeInFormula.clear();
 		cellsInMyFormula.clear();
 	}
 
+	/**
+	 * Adds a cell to the list of cells that contain this formula.
+	 * @param cell The cell to add to the list.
+	 */
 	private void addToCellsThatContainThisInFormula(Cell cell) {
 		cellsThatContainMeInFormula.add(cell);
 	}
 
+	/** Remove cell from the dependency list.
+	 *
+	 * @param cellRemoving The cell to remove from the dependency list.
+	 */
 	private void removeCellFromDependencyList(Cell cellRemoving) {
 		for (int i = 0; i < cellsInMyFormula.size(); i++) {
 			if (cellsInMyFormula.get(i).equals(cellRemoving)) {
@@ -172,6 +204,10 @@ public class Cell {
 		}
 	}
 
+	/**
+	 * Look over all the cells that have this cell in their dependency list and remove this cell.
+	 * @param theSpreadsheet The main spreadsheet.
+	 */
 	public void removeSelfFromOtherCellsDependencyList(Spreadsheet theSpreadsheet) {
 		for (int i = 0; i < cellsThatContainMeInFormula.size(); i++) {
 			Cell cellThatContainsMeInFormula = cellsThatContainMeInFormula.get(i);
@@ -184,10 +220,18 @@ public class Cell {
 		return myFormula;
 	}
 
+	/**
+	 * Returns the integer representing the row this cell is in.
+	 * @return An int representing the row this cell is in.
+	 */
 	public int getRow() {
 		return myRow;
 	}
 
+	/**
+	 * Returns the integer representing the column this cell is in.
+	 * @return An int representing the column this cell is in.
+	 */
 	public int getColumn() {
 		return myCol;
 	}
